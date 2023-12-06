5d4f6f97965f7d3ecc2088fd5aee79c8c64edfce
XIVN1987 <XIVN1987@163.com>
2023-12-06 11:27:17 +0800

    add SWM201 port

---
+++ SWM201/eeprom_emulation_port.h
@@ -0,0 +1,44 @@
+#ifndef __EEPROM_EMULATION_PORT_H__
+#define __EEPROM_EMULATION_PORT_H__
+
+#include "SWM201.h"
+
+
+#define EE_FLASH_BASE	0x7800
+
+#define EE_FLASH_END	0x8000
+
+#define EE_PAGE_SIZE	1024
+
+#define EE_ITEM_SIZE	8		// 4 字节数据、4 字节地址
+
+
+
+
+static inline void EE_FlashErase(uint32_t page)
+{
+	/* EE_PAGE_SIZE 为 1024，Flash 页大小为 512，所以需要调用 FLASH_Erase() 两次 */
+	FLASH_Erase(EE_FLASH_BASE + EE_PAGE_SIZE * page);
+	FLASH_Erase(EE_FLASH_BASE + EE_PAGE_SIZE * page + 512);
+}
+
+
+static inline void EE_FlashWrite(uint32_t page, uint32_t index, uint32_t ee_addr, uint32_t value)
+{
+	/* 先写数据后写地址，防止地址写入后数据还没来得及写入就突然掉电的情况 */
+	uint32_t buffer[2] = { value, ee_addr };
+	
+	FLASH_Write(EE_FLASH_BASE + EE_PAGE_SIZE * page + EE_ITEM_SIZE * index, buffer, EE_ITEM_SIZE / 4);
+}
+
+
+static inline void EE_FlashRead(uint32_t page, uint32_t index, uint32_t * ee_addr, uint32_t * value)
+{
+	uint32_t * pWord = (uint32_t *)(EE_FLASH_BASE + EE_PAGE_SIZE * page + EE_ITEM_SIZE * index);
+	
+	*value   = pWord[0];
+	*ee_addr = pWord[1];
+}
+
+
+#endif
